829f41680a49b5ac722990b7a68308cc5a673c47
Gowtham <gowthamkrishnag06@gmail.com>
2026-08-12 09:59:45 +0530

    Create notifications when messages are sent on an item thread

--- server/controllers/messageController.js
+++ server/controllers/messageController.js
@@ -1,4 +1,6 @@
 const Message = require('../models/Message');
+const Item = require('../models/Item');
+const Notification = require('../models/Notification');
 
 // @desc    Get discussion thread messages for an item
 // @route   GET /api/items/:itemId/messages
@@ -41,6 +43,33 @@ const sendMessage = async (req, res, next) => {
       author: req.user.name,
       body: body.trim(),
     });
+
+    // Notify the item reporter (if the sender is not the reporter)
+    const item = await Item.findById(itemId);
+    if (item && item.reportedBy.toString() !== req.user._id.toString()) {
+      await Notification.create({
+        user: item.reportedBy,
+        title: 'New message',
+        body: `${req.user.name} sent a message about your item "${item.title}".`,
+        type: 'message',
+        relatedItem: item._id,
+      });
+    }
+
+    // If the sender IS the reporter, notify all other unique participants in the thread
+    if (item && item.reportedBy.toString() === req.user._id.toString()) {
+      const previousMessages = await Message.find({ item: itemId, sender: { $ne: req.user._id } });
+      const uniqueSenders = [...new Set(previousMessages.map((m) => m.sender.toString()))];
+      for (const senderId of uniqueSenders) {
+        await Notification.create({
+          user: senderId,
+          title: 'New reply',
+          body: `${req.user.name} replied about "${item.title}".`,
+          type: 'message',
+          relatedItem: item._id,
+        });
+      }
+    }
 
     return res.status(201).json({
       _id: message._id,
